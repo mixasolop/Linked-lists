0106d76b044909740fdd3d19f87093981ae6cc35
Mykhailo Solop <mixasolop@gmail.com>
2025-02-10 14:04:39 +0300

    added Insert and Append functions

--- l.py
+++ l.py
@@ -7,5 +7,22 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.tail = None
-print("hi")
+        self.tail = None    
+    
+    def CreateNode(self, data):
+        New_Node = Node(data)
+        New_Node.prev = None
+        New_Node.next = None
+        return New_Node
+    
+    def InsertNode(self, data):
+        New_Node = self.CreateNode(data)
+        New_Node.next = self.head
+        self.head.prev = New_Node
+        self.head = New_Node
+    
+    def AppendNode(self, data):
+        New_Node = self.CreateNode(data)
+        New_Node.prev = self.tail
+        self.tail.next = New_Node
+        self.tail = New_Node
