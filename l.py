e9c804bcabcd03379e47ac7527cd07e505115ba1
Mykhailo Solop <mixasolop@gmail.com>
2025-02-10 14:11:43 +0300

    test added

--- l.py
+++ l.py
@@ -17,12 +17,32 @@ class LinkedList:
     
     def InsertNode(self, data):
         New_Node = self.CreateNode(data)
+        if(self.head == None):
+            self.head = New_Node
+            return
         New_Node.next = self.head
         self.head.prev = New_Node
         self.head = New_Node
     
     def AppendNode(self, data):
         New_Node = self.CreateNode(data)
+        if(self.tail == None):
+            self.tail = New_Node
+            return
         New_Node.prev = self.tail
         self.tail.next = New_Node
         self.tail = New_Node
+    
+    def PrintNodes(self):
+        tmp = self.head
+        while(tmp):
+            print(tmp.data)
+            tmp = tmp.next
+
+llist = LinkedList()
+print("\n")
+llist.InsertNode("a")
+llist.InsertNode("d")
+llist.InsertNode("c")
+llist.InsertNode("b")
+llist.PrintNodes()
